2f1c847dbc770fe98a30e512e84e045268c350de
Anil Kumar <anilbms75@gmail.com>
2021-05-05 06:31:09 +0200

    Add TypeScript get project settings demo

---
+++ src/typescript-sdk/project.js
@@ -0,0 +1,57 @@
+const { createClient } = require('@commercetools/sdk-client')
+const { createAuthMiddlewareForClientCredentialsFlow } = require('@commercetools/sdk-middleware-auth')
+const { createHttpMiddleware } = require('@commercetools/sdk-middleware-http')
+const { createApiBuilderFromCtpClient } = require("@commercetools/typescript-sdk");
+  
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+console.log('Getting started with commercetools Typescript SDK');
+
+const { 
+    ADMIN_CLIENT_ID,
+    ADMIN_CLIENT_SECRET,
+} = process.env;
+
+const projectKey = 'anil-fa-training-29-march-2021-dev'
+
+// Create a httpMiddleware for the your project AUTH URL
+const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
+    host: 'https://auth.europe-west1.gcp.commercetools.com',
+    projectKey,
+    credentials: {
+        clientId: ADMIN_CLIENT_ID,
+        clientSecret: ADMIN_CLIENT_SECRET,
+    },
+    scopes: ['manage_project:anil-fa-training-29-march-2021-dev'],
+    fetch,
+})
+
+// Create a httpMiddleware for the your project API URL
+const httpMiddleware = createHttpMiddleware({
+    host: 'https://api.europe-west1.gcp.commercetools.com',
+    fetch,
+})
+
+// Create a client using authMiddleware and httpMiddleware
+const client = createClient({
+    middlewares: [authMiddleware, httpMiddleware],
+})
+
+// Create a API root from API builder of commercetools platform client
+const apiRoot = createApiBuilderFromCtpClient(client);
+
+
+(async () => {
+    try {
+        await apiRoot.withProjectKey({projectKey}).get().execute()
+            .then(data => {
+                console.log('Project information --->', data);
+            })
+            .catch(error => {
+                console.log('ERROR --->', error);
+            })
+    } catch (error) {
+        console.log('ERROR --->', error);
+    }
+})();
